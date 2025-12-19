a6b934b94d2985df7f8e75e7d91b3c6d9480840a
sfq24 <songfq24@gmail.com>
2025-12-19 11:55:38 -0800

    Updated translations.ts

--- src/data/translations.ts
+++ src/data/translations.ts
@@ -44,7 +44,7 @@ export const translations = {
           name: 'Aura Living - Magnolia',
           address: 'Magnolia Neighborhood, Seattle',
           features: ['High end neighborhood', 'Walking Distance to Park'],
-          image: 'green-lake',
+          image: 'Magnolia living room.png',
         },
         {
           name: 'Aura Living - Kent',
